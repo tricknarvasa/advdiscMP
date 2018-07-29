93219d33bb387eb9c3db49af94dfbefe2e2dfd7e
Patrick James D. Narvasa <narvasa_patrick@yahoo.com.ph>
2018-07-30 02:06:34 +0800

    Init 0

--- src/Vector.java
+++ src/Vector.java
@@ -6,6 +6,8 @@ public class Vector {
 	//first constructor
 	public Vector(int dimension) {
 		this.vector = new int[dimension];
+		for(int i=0; i<dimension;i++)
+			vector[i]=0;
 	}
 	
 	//second constructor
