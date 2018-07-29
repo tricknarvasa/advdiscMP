6026f941e0f92f27a357e44cbf41880702aaeb57
Patrick James D. Narvasa <narvasa_patrick@yahoo.com.ph>
2018-07-29 23:38:19 +0800

    Vector init 0

--- src/Vector.java
+++ src/Vector.java
@@ -3,6 +3,9 @@ public class Vector {
 	int v[][];
 	 Vector (int dimension){
 		 int temp[][]= new int[dimension][dimension];
+		 for(int i=0; i<dimension; i++)
+			 for(int j=0; j<dimension; j++)
+				 temp[i][j]=0;
 		this.v= temp;
 	 }
 
