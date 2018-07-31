f572040e3097f7fe02f9d7527876cc22ea6f0c6b
ericembestro <japan569@gmail.com>
2018-07-31 20:01:24 +0800

    cleaner

--- src/Vector.java
+++ src/Vector.java
@@ -10,16 +10,8 @@ public class Vector {
 	
 	//First Constructor
 	public Vector(int dimension) {
-		//checking
-		System.out.println("Making new vector:");
-		
 		this.vector = new double[dimension];
 		this.dimension = dimension;
-		
-		//checking
-		for (int i = 0; i < dimension; i++)
-			System.out.println("Index " +i +": " +this.vector[i]);
-		System.out.println();
 	}
 	
 	//Second Constructor
@@ -27,62 +19,24 @@ public class Vector {
 		if (array.length != dimension)
 			System.out.println("array and dimension mismatch.");
 		else {
-			//checking
-			System.out.println("Making new vector:");
-			
 			this.vector = array;
 			this.dimension = dimension;
-			
-			//checking
-			for (int i = 0; i < dimension; i++)
-				System.out.println("Index " +i +": " +this.vector[i]);
-			System.out.println();
-		}
+			}
 		
 	}
 	
 	//Vector Scale
 	public Vector scale(double scalar){
-		System.out.println("Vector Scale:");
-		
-		//printing of values before scaling
-		System.out.println("Before:");
-		for (int i = 0; i < this.dimension; i++) {
-			System.out.println(this.vector[i]);
-		}
-		System.out.println();
-		
 		for (int i = 0; i < this.dimension; i++)
 			this.vector[i] *= scalar;
-		
-		//printing of values after scaling
-		System.out.println("After");
-		for (int i = 0; i < this.dimension; i++) {
-			System.out.println(this.vector[i]);
-		}
-		System.out.println();
 		
 		return this;
 	}
 	
 	//Vector Add
 	public Vector add(Vector addend) {
-		System.out.println("Vector Add:");
-		
-		//printing of values before adding
-		System.out.println("Before:");
-		for (int i = 0; i < addend.dimension; i++)
-			System.out.println(addend.vector[i]);
-		System.out.println();
-		
 		for (int i = 0; i < this.dimension; i++)
 			this.vector[i] += addend.vector[i];
-		
-		//printing of values after adding
-		System.out.println("After:");
-		for (int i = 0; i < addend.dimension; i++)
-			System.out.println(this.vector[i]);
-		System.out.println();
 		
 		return this;
 	}
